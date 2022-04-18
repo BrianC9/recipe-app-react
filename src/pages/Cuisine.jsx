637f65d563bed0b9457724cab4f2138fb4873b90
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-04-18 19:56:46 +0200

    Vegetarian Popular - Basic Route

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,6 @@
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+export default function Cuisine() {
+    const getTypeCuisine;
+	return <h1>Cuisine</h1>;
+}
